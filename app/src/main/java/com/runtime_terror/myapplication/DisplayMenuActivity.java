134a93243d273c57597b27df74d5d78fed9ca0d5
Raul Cotar <raulcotar00@gmail.com>
2019-08-21 16:44:44 +0300

    Migrated to Androidx. No problems encountered.

--- app/src/main/java/com/runtime_terror/myapplication/DisplayMenuActivity.java
+++ app/src/main/java/com/runtime_terror/myapplication/DisplayMenuActivity.java
@@ -1,13 +1,11 @@
 package com.runtime_terror.myapplication;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class DisplayMenuActivity extends AppCompatActivity {
 
