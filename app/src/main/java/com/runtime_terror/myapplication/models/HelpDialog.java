2502cead4c95d86ccef9af4ecda268107f512863
tudyfilp <tudorf90@hotmail.com>
2019-09-17 21:39:08 +0300

    added functionality for asking for help on the client side

--- app/src/main/java/com/runtime_terror/myapplication/models/HelpDialog.java
+++ app/src/main/java/com/runtime_terror/myapplication/models/HelpDialog.java
@@ -2,10 +2,14 @@ package com.runtime_terror.myapplication.models;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.runtime_terror.myapplication.R;
+import com.runtime_terror.myapplication.database.FirestoreSetup;
 
 public class HelpDialog extends Dialog {
 
@@ -32,9 +36,33 @@ public class HelpDialog extends Dialog {
         callHelpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                placeHelpRequest();
                 dismiss();
             }
         });
         show();
+    }
+
+    private void placeHelpRequest() {
+
+        final int RANDOM_TABLE_NUMBER = 213;
+        final String restaurantId = "lrApMZq9rBNLQGtzVjKa";
+        HelpOrder order = new HelpOrder(RANDOM_TABLE_NUMBER);
+
+        FirebaseFirestore db = new FirestoreSetup().getDb();
+
+        db.collection("RESTAURANTS").document(restaurantId).collection("ORDERS").add(order)
+                .addOnSuccessListener(documentReference -> {
+                    Log.d("Order id", documentReference.getId());
+                    Toast.makeText(getContext(), "Your request has been sent. Wait for the waiter to come and help you",
+                            Toast.LENGTH_LONG).show();
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("could not place order", e.toString());
+                    Toast.makeText(getContext(), "Your request could not be send. Try again later",
+                            Toast.LENGTH_LONG).show();
+                });
+
+
     }
 }
